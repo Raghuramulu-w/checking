a97136e0f592844f7d0a1a98bf34662d8bc67d60
raghuramulu-w <raghu0711@gmail.com>
2024-07-10 23:49:55 +0530

    using function

---
+++ functions1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+Id=$(id -u 0)
+VALIDATE(){
+    if [ $? -ne 0 ]
+    then 
+        echo "installation failed "
+        exit 1
+    else
+        echo "instalaltion success"
+    fi
+}
+if [ $ID -ne 0 ]
+then
+    echo "go with root user"
+    exit1
+else
+    echo "you are root user"
+fi
+yum install mysql -y
+VALIDATE
+
+yum install git -y
+VALIDATE
