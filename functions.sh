26177d640d4a55946d70fa97ea36f83ed0cb8f7c
raghuramulu-w <raghu0711@gmail.com>
2024-07-10 23:42:22 +0530

    using function

--- functions.sh
+++ functions.sh
@@ -15,3 +15,11 @@ if [ $? -ne 0 ]
     else
         echo "installation success for my sql"
 fi
+yum install git -y
+if [ $? -ne 0 ]
+then 
+  echo "installation is fail for git"
+  exit 1
+  else 
+  echo "installation is success for git"
+  fi
