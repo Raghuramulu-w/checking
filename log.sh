5a998e1be765fe0c0470cdda92e71cee18898af5
raghuramulu-w <raghu0711@gmail.com>
2024-07-11 13:37:32 +0530

    hai

--- log.sh
+++ log.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-echo "your script name is $0"
-echo "how many args passed $#"
-echo "all args $@"
+ID=$(id -u)
+VALIDATE(){
+    if [$? -ne 0 ]
+    then 
+        echo "ERROR : installation fail"
+    else
+        echo "installation success"
+    fi
+}
+if [$ID -ne 0]
+then 
+    echo "proceed with root user"
+    exit 1
+else
+    echo "you are root user welcome"
+fi
+    yum install mysql -y
+    VALIDATE $? "my installation"
