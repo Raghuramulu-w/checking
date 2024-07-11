8e5128b706bcb4ee2523c326007b870fcbb125ee
raghuramulu-w <raghu0711@gmail.com>
2024-07-11 14:28:58 +0530

    hai

--- gitcheck.sh
+++ gitcheck.sh
@@ -2,7 +2,7 @@
 NUMBER=$1
 if [ $NUMBER -gt 20 ]
 then 
-   echo -e "given number is greater than \e[31 20"
+   echo -e "given number is greater than \e[31m 20"
 else 
    echo "given number is not greater than 20 "
 
