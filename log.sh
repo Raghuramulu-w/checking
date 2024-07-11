24f0e3692cedb1e81dba7c77490e5a11aa4f787b
raghuramulu-w <raghu0711@gmail.com>
2024-07-11 14:34:38 +0530

    hai

--- log.sh
+++ log.sh
@@ -3,10 +3,10 @@ ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 #echo "$TIMESTAMP"
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
-R=\e[31m
-G=\e[32m
-Y=\e[33m
-N=\e[0m
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
