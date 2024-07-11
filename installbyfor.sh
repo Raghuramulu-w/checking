66e723e8678cce5d99be93c44e7aca45ddd944b3
raghuramulu-w <raghu0711@gmail.com>
2024-07-12 00:00:11 +0530

    hai

--- installbyfor.sh
+++ installbyfor.sh
@@ -9,9 +9,9 @@ LOGFILE="/tmp/$0-$TIMESTAMP.log"
 VALIDATE(){
   if [ $1 -ne 0 ]
   then 
-    echo -e " $R ERROR $2 $package failed "
+    echo -e " $R ERROR $2 failed "
   else
-    echo -e " $2 $package is $G success  "
+    echo -e " $2 is $G success  "
   fi
 }
 if [ $ID -ne 0 ]
@@ -19,7 +19,7 @@ if [ $ID -ne 0 ]
       echo "do with root user"
       exit 1
     else 
-      echo "you are root user"
+      echo -e "$G you are root user"
 fi
 for package in $@
 do
@@ -27,12 +27,11 @@ do
     if [ $? -ne 0 ]
      then 
      yum install $package &>> $LOGFILE
-     VALIDATE $? "MY INSTALLATION  "
+     VALIDATE $? "MY INSTALLATION $package  "
      else
      echo -e " $Y $package is already installed  SKIPPING" 
      #VALIDATE $? "my installation $package "
      fi
-
 done
 
 
