7f4e43e36e54d5a276fe4bfdddefd6e2c45f5f54
raghuramulu-w <raghu0711@gmail.com>
2024-07-11 23:00:27 +0530

    hai

--- installbyfor.sh
+++ installbyfor.sh
@@ -7,9 +7,9 @@ N="\e[0m"
 VALIDATE(){
   if [ $1 -ne 0 ]
   then 
-    echo -e " ERROR $2 \e[31m failed "
+    echo -e " ERROR $2 $package \e[31m failed "
   else
-    echo -e " $2 \e[32m success \e[0m "
+    echo -e " $2 $package \e[32m success \e[0m "
   fi
 }
 if [ $ID -ne 0 ]
